d095ceebce33568e55f57613c5269e73341f8799
jeongho <qkrjh0904@gmail.com>
2023-11-09 13:55:46 +0900

    git init

---
+++ src/test/kotlin/com/fc/fcboard/FcBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fc.fcboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FcBoardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
